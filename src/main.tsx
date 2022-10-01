f24d0d06a08c1c446fbb495b823e825e7c5933b7
rodrocha444 <rodrocha444@gmail.com>
2022-10-01 12:25:58 -0300

    ➕ Apollo Client and Graphql added

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
-import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ChakraProvider } from '@chakra-ui/react'
+
 import { App } from './App'
+import { ApolloProvider } from '@apollo/client/react'
+
+const client = new ApolloClient({
+  uri: '',
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>7
+  <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </ChakraProvider>
   </React.StrictMode>
 )
